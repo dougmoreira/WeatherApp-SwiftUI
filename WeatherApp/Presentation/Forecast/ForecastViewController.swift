41b77cd8644976f9930b145749036fafdc05d643
Douglas Moreira <33124673+dougmoreira@users.noreply.github.com>
2023-09-18 17:29:13 -0300

    displaying temperature on its label

--- WeatherApp/Presentation/Forecast/ForecastViewController.swift
+++ WeatherApp/Presentation/Forecast/ForecastViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 protocol ForecastDisplayLogic: AnyObject {
-    func displayForecast(with temeprature: Double)
+    func displayForecast(with temperature: Double)
 }
 
 class ForecastViewController: UIViewController {
@@ -16,8 +16,13 @@ class ForecastViewController: UIViewController {
     
     public init(interactor: ForecastBusinessLogic) {
         self.interactor = interactor
-        
         super.init(nibName: nil, bundle: nil)
+        
+        self.view.addSubview(stack)
+        
+        stack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        stack.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
     }
     
     required init?(coder: NSCoder) {
@@ -28,12 +33,27 @@ class ForecastViewController: UIViewController {
         interactor.getCurrentForecast()
         super.viewDidLoad()
     }
+    
+    private lazy var stack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [temperatureLabel])
+        stack.alignment = .center
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
 }
 
 extension ForecastViewController: ForecastDisplayLogic {
-    func displayForecast(with teperature: Double) {
-        print(teperature)
+    func displayForecast(with temperature: Double) {
+        DispatchQueue.main.async { [weak self] in
+            self?.temperatureLabel.text = "\(temperature)"
+        }
     }
 }
 
