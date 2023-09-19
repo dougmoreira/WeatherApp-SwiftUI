82572e6f8aea95eefd171296b0e64f29cd9c522a
Douglas Moreira <33124673+dougmoreira@users.noreply.github.com>
2023-09-19 16:30:27 -0300

    testing presenter

---
+++ WeatherAppTests/Presentation/ForecastPresenterTests.swift
@@ -0,0 +1,33 @@
+//
+//  ForecastPresenterTests.swift
+//  WeatherAppTests
+//
+//  Created by Douglas Moreira on 19/09/23.
+//
+
+import XCTest
+@testable import WeatherApp
+
+final class ForecastPresenterTests: XCTestCase {
+    private let viewControllerSpy = ForecastDisplayLogicSpy()
+    private lazy var sut = ForecastPresenter()
+    
+    func test_presentForecast_shouldCallViewControllerWithCorrectParams() {
+        let temperature: Double = 0
+        
+        sut.viewController = viewControllerSpy
+        sut.presentForecast(with: temperature)
+        
+        XCTAssertEqual(viewControllerSpy.displayForecastTemperaturePassed, temperature)
+        XCTAssertEqual(viewControllerSpy.displayForecastCallCount, 1)
+    }
+    
+    func test_presentForecastError_shouldCallViewControllerWithCorrectParams() {
+        sut.viewController = viewControllerSpy
+        sut.presentForecastError()
+        
+        XCTAssertEqual(viewControllerSpy.displayForecastErrorCallCount, 1)
+    }
+
+
+}
