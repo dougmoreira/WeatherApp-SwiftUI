12c4d848bd905fcbc1d30b74865915af753cf382
Douglas Moreira <33124673+dougmoreira@users.noreply.github.com>
2023-09-25 20:11:26 -0300

    creating carouselView

--- WeatherApp/Presentation/Forecast/View/ForecastView.swift
+++ WeatherApp/Presentation/Forecast/View/ForecastView.swift
@@ -20,7 +20,7 @@ struct ForecastView: View {
         VStack {
             switch viewModel.viewState {
             case .content(let temperature):
-                TemperatureView(viewModel: viewModel, temperature: temperature)
+                TemperatureView(temperature: temperature)
             case .error:
                 ForecastErrorView()
             case .loading:
@@ -30,7 +30,7 @@ struct ForecastView: View {
     }
 }
 
-struct TemperatureView: View {
+struct TemperatureViewLegacy: View {
     @ObservedObject var viewModel: ForecastViewModel
     var temperature: Double
     
@@ -60,6 +60,40 @@ struct ForecastLoadingView: View {
     var body: some View {
         VStack {
             ProgressView()
+        }
+    }
+}
+
+struct TemperatureView: View {
+    var temperature: Double
+
+    @State private var items: [CarouselItem] = [
+        .init(color: .red, title: "Belo Horizonte", subTitle: "Predominantemente nublado"),
+        .init(color: .blue, title: "São Paulo", subTitle: "Predominantemente Limpo"),
+    ]
+
+    var body: some View {
+        VStack {
+            CarouselView(
+                data: $items
+            ) { $item in
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(item.color.gradient)
+                    .frame(width: nil, height: 220)
+            } titleContent: { $item in
+                VStack(spacing: 5) {
+                    Text(item.title)
+                        .font(.largeTitle.bold())
+                    
+                    Text(item.subTitle)
+                        .foregroundStyle(.gray)
+                        .multilineTextAlignment(.center)
+                        .frame(height: 45)
+                }
+                .padding(.bottom, 35)
+            }
+            .safeAreaPadding([.horizontal, .top], 35)
+            
         }
     }
 }
